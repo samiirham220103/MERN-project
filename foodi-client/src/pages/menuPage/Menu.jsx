b815106212266991d47e4a2b65e180993486aa9c
sami irhamnillah <samiirhamnillah03@gmai.com>
2023-12-22 14:30:26 +0700

    server

--- foodi-client/src/pages/menuPage/Menu.jsx
+++ foodi-client/src/pages/menuPage/Menu.jsx
@@ -14,7 +14,9 @@ const Menu = () => {
     // Fetch data from the backend
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:6001/menu");
+        const response = await fetch(
+          "https://mern-project-server-ten.vercel.app/menu"
+        );
         const data = await response.json();
         setMenu(data);
         setFilteredItems(data); // Initially, display all items
@@ -40,7 +42,7 @@ const Menu = () => {
   const showAll = () => {
     setFilteredItems(menu);
     setSelectedCategory("all");
-    setCurrentPage(1); 
+    setCurrentPage(1);
   };
 
   const handleSortChange = (option) => {
@@ -71,14 +73,13 @@ const Menu = () => {
     setCurrentPage(1);
   };
 
-//   console.log(filteredItems);
+  //   console.log(filteredItems);
   // Pagination logic
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
 
   return (
     <div>
@@ -105,8 +106,7 @@ const Menu = () => {
       {/* menu shop  */}
       <div className="section-container">
         <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
-          
-           {/* all category buttons */}
+          {/* all category buttons */}
           <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4  flex-wrap">
             <button
               onClick={showAll}
@@ -146,7 +146,7 @@ const Menu = () => {
             </button>
           </div>
 
-            {/* filter options */}
+          {/* filter options */}
           <div className="flex justify-end mb-4 rounded-sm">
             <div className="bg-black p-2 ">
               <FaFilter className="text-white h-4 w-4" />
@@ -174,9 +174,11 @@ const Menu = () => {
         </div>
       </div>
 
-       {/* Pagination */}
-       <div className="flex justify-center my-8 flex-wrap gap-2">
-        {Array.from({ length: Math.ceil(filteredItems.length / itemsPerPage) }).map((_, index) => (
+      {/* Pagination */}
+      <div className="flex justify-center my-8 flex-wrap gap-2">
+        {Array.from({
+          length: Math.ceil(filteredItems.length / itemsPerPage),
+        }).map((_, index) => (
           <button
             key={index + 1}
             onClick={() => paginate(index + 1)}
